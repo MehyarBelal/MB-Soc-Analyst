bbd5b0f7fde188616ec9e520e23ac36629595a6c
Mehyar Belal <76401890+MehyarBelal@users.noreply.github.com>
2025-04-25 19:49:53 +1000

    v

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,11 @@
 alert("Bonjor")
+
+const buttons = document.querySelectorAll('.toggle-btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      const isActive = button.classList.toggle('active');
+      content.style.display = isActive ? 'block' : 'none';
+    });
+  });
